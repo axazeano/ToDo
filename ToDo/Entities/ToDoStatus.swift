693faec3b1e222cb89300ec9e0040a6b5f81c8dc
Kubantsev Vladimir <kubantsev.vladimir@gmail.com>
2018-12-22 16:01:37 +0300

    Create initial entities

---
+++ ToDo/Entities/ToDoStatus.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoStatus.swift
+//  ToDo
+//
+//  Created by Владимир Кубанцев on 22/12/2018.
+//  Copyright © 2018 VoInc. All rights reserved.
+//
+
+import Foundation
+
+enum ToDoStatus: String, Codable {
+    case new = "new"
+    case inProgress = "inProgress"
+    case completed = "completed"
+}
