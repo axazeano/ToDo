9558bfac6816e15d8e71080c3e566f3b06137b68
Kubantsev Vladimir <kubantsev.vladimir@gmail.com>
2018-12-22 15:43:39 +0300

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Владимир Кубанцев on 22/12/2018.
+//  Copyright © 2018 VoInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
