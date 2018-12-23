01e31d7e148f5403e0a8d3f691fac7819db06fb1
Kubantsev Vladimir <kubantsev.vladimir@gmail.com>
2018-12-24 00:18:42 +0300

    Add a basic assembly

--- ToDo/Modules/TasksList/Assembly/TasksListAssembly.swift
+++ ToDo/Modules/TasksList/Assembly/TasksListAssembly.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+final class TasksListAssembly {
+    func assembly(with view: TasksListView) {
+        
+        let presenter = TaskListPresenter()
+        let interactor = TasksListInteractor(
+            storeService: InMemoryStoreService()
+        )
+        
+        view.output = presenter
+        presenter.view = view
+        presenter.interactor = interactor
+        interactor.output = presenter
+    }
+}
