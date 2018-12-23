36975a56d20e77a44b8828660a186b3f22ffe673
Kubantsev Vladimir <kubantsev.vladimir@gmail.com>
2018-12-24 00:14:14 +0300

    Implement a dirty presenter

--- ToDo/Modules/TasksList/Presenter/TasksListPresenter.swift
+++ ToDo/Modules/TasksList/Presenter/TasksListPresenter.swift
@@ -7,3 +7,82 @@
 //
 
 import Foundation
+
+final class TaskListPresenter {
+    weak var view: TasksListView?
+    var interactor: TasksListInteractorInput?
+    private let dateFormatter = DateFormatter()
+    
+    init() {
+        dateFormatter.dateFormat = "d MMMM"
+    }
+}
+
+extension TaskListPresenter: TasksListViewOutput {
+    func loaded() {
+        interactor?.requestTasks()
+    }
+    
+    func appeared() {
+        
+    }
+    
+    func requestCreateTask() {
+        
+    }
+    
+    func requestConfigureFilter() {
+        
+    }
+    
+    
+}
+
+extension TaskListPresenter: TasksListInteractorOutput {
+    func set(tasks: [ToDoItem]) {
+        let viewModel = convertTasksToViewModel(tasks)
+        view?.set(viewModel: viewModel)
+    }
+    
+    func set(tasks: [ToDoItem], with status: ToDoStatus) {
+        
+    }
+    
+    func setErrorState() {
+        
+    }
+    
+    private func convertTasksToViewModel(_ tasks: [ToDoItem]) -> TasksListViewModel {
+        let uniqueDates = Set(tasks.compactMap { $0.createAt })
+        let sections = uniqueDates.map { (date) -> TasksListGroup in
+            let groupedTasks = tasks.filter { $0.createAt == date }
+            return converTasksToViewSection(groupedTasks, date: date)
+        }
+        return TasksListViewModel(groups: sections)
+    }
+    
+    private func converTasksToViewSection(_ tasks: [ToDoItem], date: Date) -> TasksListGroup {
+        return TasksListGroup(
+            title: dateFormatter.string(from: date),
+            items: tasks.map { self.converTaskToViewItem($0) }
+        )
+    }
+    
+    private func converTaskToViewItem(_ task: ToDoItem) -> TasksListItem {
+        return TasksListItem(
+            title: task.title,
+            status: getTitle(for: task.status),
+            note: task.note,
+            dueDate: dateFormatter.string(from: task.createAt),
+            onTapHandler: { }
+        )
+    }
+    
+    private func getTitle(for status: ToDoStatus) -> String {
+        switch status {
+        case .new: return "Новая"
+        case .inProgress: return "В процессе"
+        case .completed: return "Завершена"
+        }
+    }
+}
