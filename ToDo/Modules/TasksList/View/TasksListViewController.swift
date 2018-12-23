f02b7046a615548fb32f9fe1c4446231e5986a4e
Kubantsev Vladimir <kubantsev.vladimir@gmail.com>
2018-12-23 23:48:10 +0300

    Fix a view protocols and implement dirty view

--- ToDo/Modules/TasksList/View/TasksListViewController.swift
+++ ToDo/Modules/TasksList/View/TasksListViewController.swift
@@ -10,6 +10,114 @@ import Foundation
 import UIKit
 import SnapKit
 
-final class TasksListViewController: UIViewController {
+final class TasksListViewController: UIViewController, TasksListView {
+    var output: TasksListViewOutput?
     
+    private let tableView = UITableView(frame: .zero, style: .grouped)
+    private let reuseIdentifier = "TaskCell"
+    private var viewModel: TasksListViewModel?
+    
+    private func embedViews() {
+        view.addSubview(tableView)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        embedViews()
+        setupLayout()
+        setupAppearance()
+        setupBehaviour()
+        output?.loaded()
+    }
+    
+    private func setupLayout() {
+        tableView.snp.makeConstraints { (make) in
+            make.leading.equalTo(view.snp.leading)
+            make.top.equalTo(view.snp.top)
+            make.leading.equalTo(view.snp.leading)
+            make.bottom.equalTo(view.snp.bottom)
+        }
+    }
+    
+    private func setupAppearance() {
+        tableView.backgroundColor = .white
+    }
+    
+    private func setupBehaviour() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(
+            TaskListTableViewCell.self,
+            forCellReuseIdentifier: reuseIdentifier
+        )
+    }
 }
+
+extension TasksListViewController: TasksListViewInput {
+    func set(viewModel: TasksListViewModel) {
+        self.viewModel = viewModel
+        tableView.reloadData()
+    }
+    
+    func showOnDeleteAlert(
+        okAction: @escaping () -> Void,
+        cancelAction: @escaping () -> Void
+    ) {
+        
+    }
+}
+
+extension TasksListViewController: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return viewModel?.groups[section].items.count ?? 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel?.groups.count ?? 0
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        
+        guard let viewModel = viewModel else {
+            return UITableViewCell()
+        }
+        
+        guard
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: reuseIdentifier
+            ) as? TaskListTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        
+        let viewModelForCell = viewModel.groups[indexPath.section].items[indexPath.row]
+        
+        cell.apply(viewModel: viewModelForCell)
+        return cell
+    }
+    
+    func tableView(
+        _ tableView: UITableView,
+        titleForHeaderInSection section: Int
+    ) -> String? {
+        
+        return viewModel?.groups[section].title
+    }
+}
+
+extension TasksListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let viewModel = viewModel else {
+            return
+        }
+        viewModel.groups[indexPath.section].items[indexPath.row].onTapHandler()
+    }
+}
+
+
