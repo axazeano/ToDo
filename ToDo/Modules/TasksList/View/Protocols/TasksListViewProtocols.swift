cb742b0c76def8bd4edb91c5d8a432423fcc0860
Kubantsev Vladimir <kubantsev.vladimir@gmail.com>
2018-12-23 21:06:48 +0300

    Describe View models and protocols for a view layer

--- ToDo/Modules/TasksList/View/Protocols/TasksListViewProtocols.swift
+++ ToDo/Modules/TasksList/View/Protocols/TasksListViewProtocols.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+protocol TasksListViewInput: class {
+    func set(viewModel: [TasksListViewModel])
+    
+    func showOnDeleteAlert(
+        okAction: @escaping ()->Void,
+        cancelAction: @escaping ()->Void
+    )
+}
+
+protocol TasksListViewOutput: class {
+    func loaded()
+    func appeared()
+    func requestCreateTask()
+    func requestConfigureFilter()
+}
+
+protocol TasksListView: TasksListViewInput {
+    var output: TasksListViewOutput? { get set }
+}
