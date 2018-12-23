0a45582356df0968a4a3d7cdd1044b66857eff14
Kubantsev Vladimir <kubantsev.vladimir@gmail.com>
2018-12-23 21:30:08 +0300

    Dirty implementation for an interactor

--- ToDo/Modules/TasksList/Interactor/TasksListInteractor.swift
+++ ToDo/Modules/TasksList/Interactor/TasksListInteractor.swift
@@ -8,3 +8,44 @@
 
 import Foundation
 
+final class TasksListInteractor {
+    weak var output: TasksListInteractorOutput?
+    var storeService: StoreService
+    
+    init(storeService: StoreService) {
+        self.storeService = storeService
+    }
+}
+
+extension TasksListInteractor: TasksListInteractorInput {
+    func requestTasks() {
+        storeService.loadItems(
+            onSuccess: { [weak self] (tasks) in
+                self?.output?.set(tasks: tasks)
+            }) { (error) in
+                print(error)
+            }
+    }
+    
+    func requestTasks(with status: ToDoStatus) {
+        storeService.loadItems(
+            onSuccess: { [weak self] (tasks) in
+                self?.output?.set(
+                    tasks: tasks.filter { $0.status == status },
+                    with: status
+                )
+        }) { (error) in
+            print(error)
+        }
+    }
+    
+    func remove(task: ToDoItem) {
+        storeService.remove(
+            item: task,
+            onSuccess: {
+                
+        }) { (error) in
+            
+        }
+    }
+}
