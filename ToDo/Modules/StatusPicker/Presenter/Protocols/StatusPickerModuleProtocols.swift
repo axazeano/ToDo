078d5f2ad7506a773dd03306c02cfc0256597c08
Kubantsev Vladimir <kubantsev.vladimir@gmail.com>
2018-12-24 16:46:58 +0300

    Add presenter and other staff

---
+++ ToDo/Modules/StatusPicker/Presenter/Protocols/StatusPickerModuleProtocols.swift
@@ -0,0 +1,13 @@
+//
+//  StatusPickerModuleProtocols.swift
+//  ToDo
+//
+//  Created by Владимир Кубанцев on 24/12/2018.
+//  Copyright © 2018 VoInc. All rights reserved.
+//
+
+import Foundation
+
+protocol StatusPickerModuleOutput: class {
+    func set(status: ToDoStatus?)
+}
